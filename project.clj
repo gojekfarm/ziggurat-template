7589c29edeaccd65ea6e9522a67bac278e509465
Anirudh <anirudh2403@gmail.com>
2019-02-25 19:55:00 +0530

    Updates project.clj with description and license

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject ziggurat/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "This is a template for applications that use ziggurat for processing data streams from kafka."
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :eval-in-leiningen true)
